1ee3718eaf0d2663d1206be4362d762ff2998783
nilarrus <nilarrus69@gmail.com>
2018-09-28 19:44:29 +0200

    Formularis simples

--- formularis/processa_dades.php
+++ formularis/processa_dades.php
@@ -1,2 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<pre>
+	<?php
+$the_request;
+switch($_SERVER['REQUEST_METHOD'])
+{
+	case 'POST': 
+		$the_request = &$_POST; 
+	break;
+	
+	default:
+
+	break;
+}
+print_r($the_request);
+foreach ($the_request as $key => $value) {
+	echo "Cosa: $key <br/>";
+	if(gettype($value)=="array"){
+		echo "Contenido(s): ";
+		foreach ($value as $i){
+			echo "$i, ";
+		}
+	}else{
+		echo "Contenido: $value ";
+	}
+	echo "<br/>";
+}
 ?>
+</pre>
+</body>
+</html>
