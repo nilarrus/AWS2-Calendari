8301eea67147bf484e36ee3bc87e0f78005aad8e
nilarrus <nilarrus69@gmail.com>
2018-10-05 19:32:19 +0200

    pizza final

--- pizza/index.php
+++ pizza/index.php
@@ -9,7 +9,7 @@
 		<h2>Pizza Seleccion:</h2>
 		<form action="" method="post" id="myform" name="myForm">
 			<label>Seleciona una Massa la </label>
-			<select name="mySelect" id="">
+			<select name="Massa" id="">
 				<optgroup label="Massas">
 					<option value="Pan">
 						Pan
@@ -23,37 +23,43 @@
 				</optgroup>
 			</select>
 			</br><h3>Ingredientes:</h3>
-			<input type="radio" name="myRadio" value="Jamon" />Jamon
-			<input type="radio" name="myRadio" value="Oregano"/>Oregano
-			<input type="radio" name="myRadio" value="Pina">Piña
+			<input type="checkbox" name="ingrediente[]" value="Jamon"  />Jamon
+			<input type="checkbox" name="ingrediente[]" value="Oregano" checked />Oregano
+			<input type="checkbox" name="ingrediente[]" value="Piña">Piña
+			<input type="checkbox" name="ingrediente[]" value="Hamburgesa">Hamburgesa
 			</br>
 			<button type="submit" id="myButton" >Calcular Precio
 			</button>
 		</form>
 	</div>
 	<div>
-		<?php
-		$the_request;
-		$ingredientes;
-		switch($_SERVER['REQUEST_METHOD'])
-		{
-			case 'POST': 
-				$the_request = &$_POST; 
-			break;
-			
-			default:
+	<?php
+		$pFinal=5;
+		$con=1;
+		$noHay=0;
+		if((gettype($_POST['ingrediente'])=="NULL")&&(gettype($_POST['Massa'])=="NULL")){
 
-			break;
-		}
-		foreach ($the_request as $key => $value) {
-			if($key=="mySelect"){
-				foreach ($value as $i ) {
-					echo "Massa Seleccionada : $i";
+		}else{
+			echo "<p>La Massa seleccionada es: ".$_POST['Massa']."</p>";
+			$ingredientes= $_POST['ingrediente'];
+			echo "Ingredientes seleccionados son: ";
+			foreach ($ingredientes as $key) {
+				if ($key =="Oregano") {
+					$noHay=1;
 				}
+				echo "$key  ";
+				$con++;
 			}
+			if($noHay==0){
+				echo"<h1 style='color:red'>Falta seleccionar el Oregano</h1>";		
+			}else{
+				$pFinal = $pFinal +(0.5*$con);
+				echo "<hr>";
+				echo "</br><h3 style='color:green'> Precio Final: $pFinal €</h3>";
 		}
-
+			
+		}
 		?>
-	</div>
+		</div>
 </body>
 </html>
